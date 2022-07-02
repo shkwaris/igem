3a7ba85c6ce38e39d813d39d736c49b16446680d
Nterekis Grigoris <grigorisnterekis@gmail.com>
2022-07-02 21:25:16 +0300

    Adding favicon.png.

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'https://static.igem.wiki/teams/4118/wiki/favicon.png' }
     ]
   },
 
