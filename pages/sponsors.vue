20e3413fdd1229c33535761f752638d36a447bc8
Spyros Derekis <spyder@radial.gr>
2022-10-12 12:16:19 +0300

    Make sponsors page and footer

--- pages/sponsors.vue
+++ pages/sponsors.vue
@@ -1,23 +1,65 @@
 <template>
     <div>
       <Header :title="currentPage"></Header>
-  
+
       <div class="bg" style="background: url('https://static.igem.wiki/teams/4118/wiki/website-assets/rectangle-hero.png') center center no-repeat; background-size: cover;">
         <h1 class="head-title">{{currentPage}}</h1>
       </div>
-  
+
       <section class="intro">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p style="text-align: center">We want to express our gratitude and thank our sponsors. Without them none of it would have been possible.</p>
+      </section>
+
+      <section class="container sponsors-content">
+        <div class="row">
+          <div class="col-xs-12">
+            <img class="rgcc" src="https://static.igem.wiki/teams/4118/wiki/website-assets/sponsors/logo-rgcc-white.png" alt="RGCC" />
+            <p>RGCC was launched in 2004 by genetics pioneer Dr Ioannis Papasotiriou who believes that the key to effectively treating cancer lies in personalized medicine using the information in a patient’s genes. Using world-leading technology, equipment and innovative techniques, our team of scientists have developed a range of tests that give healthcare professionals comprehensive information about a patient’s genetics, physiology and immune profiles. This diversion from a ‘one-size-fits all approach’ uses data to determine what treatment will work best. Most importantly, by looking at a person’s genetics, physiology and immune profiles, our cost-effective and highly accurate tests will give you the information you need to formulate effective personalized treatment plans.</p>
+            <a href="https://www.rgcc-group.com" class="sponsors-link" target="_blank">https://www.rgcc-group.com</a>
+          </div>
+
+          <div class="col-xs-12">
+            <img class="naco" src="https://static.igem.wiki/teams/4118/wiki/website-assets/sponsors/naco-logo.png" alt="Naco" />
+            <p>The company with the distinctive title NACO was founded in February 1992 with headquarters in Patras and is the Authorized Representative of XEROX HELLAS. The company's main activity is the sale and maintenance of XEROX HELLAS products, which concern machines and integrated systems that can meet the needs of each customer. The company also provides commercial and public service customers with a maintenance contract to fully cover their equipment.</p>
+            <a href="https://www.naco.gr/" class="sponsors-link" target="_blank">https://www.naco.gr/</a>
+          </div>
+
+          <div class="col-xs-12">
+            <img class="patrasuniversity" src="https://static.igem.wiki/teams/4118/wiki/website-assets/sponsors/patras-university-logo.png" alt="Patras University" />
+            <p>Integrated DNA Technologies, Inc. (IDT) is in the business of moving science forward. We are scientists working for scientists. Our innovative tools and solutions for genomics applications are breaking down barriers and inspiring you to dream big and achieve your next breakthroughs.</p>
+            <a href="https://eu.idtdna.com/pages" class="sponsors-link" target="_blank">https://eu.idtdna.com/pages</a>
+          </div>
+
+          <div class="col-xs-12">
+            <img class="idt" src="https://static.igem.wiki/teams/4118/wiki/website-assets/sponsors/idt-logo.png" alt="IDT" />
+            <p>Integrated DNA Technologies, Inc. (IDT) is in the business of moving science forward. We are scientists working for scientists. Our innovative tools and solutions for genomics applications are breaking down barriers and inspiring you to dream big and achieve your next breakthroughs.</p>
+            <a href="https://eu.idtdna.com/pages" class="sponsors-link" target="_blank">https://eu.idtdna.com/pages</a>
+          </div>
+
+          <div class="col-xs-12">
+            <img class="neb" src="https://static.igem.wiki/teams/4118/wiki/website-assets/sponsors/neb-logo.png" alt="NEB" />
+            <p>Created "by scientists for scientists", NEB is renowned for consistently providing exceptional product quality and unsurpassed technical support. For over four decades, NEB has been shaping the landscape of bioscience research by discovering, developing and supporting superior research reagents. From our founding principles – placing the advancement of science and the stewardship of the environment as our highest priorities – to our unique corporate culture, NEB’s philosophy can be distilled down to three core values: passion, humility and being genuine.
+              <br><br>
+            A supplier-of-choice for scientists across the globe, NEB offers the largest selection of recombinant and native enzymes for genomic research. While restriction enzymes remain part of our core product portfolio, our ever-expanding catalog also includes products related to PCR, gene expression, sample preparation for next generation sequencing, synthetic biology, glycobiology, epigenetics and RNA analysis. Additionally, NEB is focused on strengthening alliances that enable new technologies to reach key market sectors, including molecular diagnostics development.</p>
+            <a href="https://www.snapgene.com/" class="sponsors-link" target="_blank">https://www.snapgene.com/</a>
+          </div>
+
+          <div class="col-xs-12">
+            <img class="snapgene" src="https://static.igem.wiki/teams/4118/wiki/website-assets/sponsors/snapgene-logo.png" alt="SnapGene" />
+            <p>In 2004, Benjamin Glick co-founded SnapGene to eliminate the inefficiencies he observed in molecular biology research. Created by a team of scientists, software engineers, and usability experts, SnapGene has become the easiest and most secure way for scientists to plan, visualize and document everyday molecular biology procedures.</p>
+            <a href="https://international.neb.com/about-neb/neb-overview" class="sponsors-link" target="_blank">https://international.neb.com/about-neb/neb-overview</a>
+          </div>
+        </div>
       </section>
     </div>
 </template>
-  
+
   <script>
   export default {
       data() {
           return {
               currentPage: "Sponsors",
           };
-      }, 
+      },
   }
   </script>
