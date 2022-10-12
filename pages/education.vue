cbf69285420a52869908eed5f345533b927b9a7e
georgeandr <giorgosandronikidis@hotmail.com>
2022-10-12 04:36:38 +0300

    second to last

--- pages/education.vue
+++ pages/education.vue
@@ -27,6 +27,7 @@
                 <li><a :href="`${currentPage}/#section4`" v-scroll-to="'#section4'">Summer Camp</a></li>
                 <li><a :href="`${currentPage}/#section5`" v-scroll-to="'#section5'">Presentation at School</a></li>
                 <li><a :href="`${currentPage}/#section6`" v-scroll-to="'#section6'">New Chapter</a></li>
+                <li><a :href="`${currentPage}/#section7`" v-scroll-to="'#section7'">SynBio Achievements</a></li>
               </ol>
             </div>
 
@@ -37,6 +38,10 @@
           <section id="section1">
             <h2 class="main-title">Comic</h2>
             <p>Our team has an unwavering goal to fight cancer. As we know, awareness and information are the key to preventing and fighting this morbidity. Therefore, starting from childhood, we decided to create a comic that will bring children closer to our idea and our project. After all, knowledge is power.This is how we bring young people into contact with health, wellness, cancer and Igemers through a delightful story. Our comic made the rounds of Greek schools where the children had the opportunity to hear our story and read it. The comic is called the lamp of health.</p>
+            <div class="gallery grid">
+              <a href="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimageglafu2.gif"><img src="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimageglafu2.gif"/></a>
+              <a href="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimagen0y8fc.gif"><img src="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimagen0y8fc.gif"/></a>
+            </div>
           </section>
           <section id="section2">
             <h2 class="main-title">Board Games</h2>
@@ -48,6 +53,9 @@
               <h3 class="small-title">GENE-US</h3>
               <p>This is a knowledge board game in the form of questions and answers which consists of 6 categories of cards and combines knowledge of synthetic biology, anatomy, or physiology while including a more specialized category for the most daring players!</p>
             </section>
+            <figure class="gallery">
+              <a href="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimages3lbpl.gif"><img src="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimages3lbpl.gif"><</a>
+            </figure>
           </section>
           <section id="section3">
             <h2 class="main-title">The SynBio Dictionary</h2>
@@ -62,6 +70,9 @@
             <p>Thus, they knew to evaluate our work, to point out any changes to us, but most importantly, for the students to learn some initial information about synthetic biology.</p>
             <p>We learned two things from this first meeting. On the one hand, we can achieve the dissemination of synthetic biology more playfully to a portion of people who are not familiar with it. But on the other hand, we learned first-hand that knowledge is not one-sided and that the two teams within the framework of our cooperation had to learn from each other.</p>
             <p>We learned about tabletop design from the students' experience and Mr. Arlapanos, and those of us got a little taste of the concept of SynBio.</p>
+            <div class="gallery grid">
+              <a href="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimageretzsa.gif"><img src="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimageretzsa.gif"></a>
+            </div>
           </section>
           <section id="section5">
             <h2 class="main-title">Presentation at School</h2>
@@ -71,10 +82,21 @@
             <p>Furthermore, through the comic book "The Lamp of Health," we helped them to understand in a simple but at the same time explanatory way that many of our choices can affect our lives positively or negatively. Finally, we were given the opportunity to contribute to the vocational guidance of young people, considering that we are undergraduate students 
               in the field of health sciences, participating in a synthetic biology competition and dealing with such a combustive issue.</p>
             <p>Overall from our point of view, we feel that we have been a positive influence on the students, and our meetings have been efficacious.</p>
+            <div class="gallery grid">
+              <a href="https://static.igem.wiki/teams/4118/wiki/website-assets/education/presentation-at-school.jpg"><img src="https://static.igem.wiki/teams/4118/wiki/website-assets/education/presentation-at-school.jpg"></a>
+              <a href="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimageqxrqkj.gif"><img src="https://static.igem.wiki/teams/4118/wiki/website-assets/education/tempimageqxrqkj.gif"></a>
+            </div>
           </section>
           <section id="section6">
             <h2 class="main-title">New Chapter</h2>
             <p>After we had successfully visited schools and the summer camp, we got impressed by the interest students showed in our project and most importantly in Synthetic Biology. The majority of them expressed their desire to further study and explore this scientific field. We thought that it would be really helpful to integrate Synthetic Biology to their lessons’ schedule. For this reason one of our team members, our advisor, wrote a whole chapter in which Synthetic Biology is scientifically defined and further approaches and biosecurity are described in detail. This chapter is a proposed extension for the school book of high school students interested in this field, so that they will  have the opportunity to be taught more about Synthetic Biology during their school life.</p>
+            <a class="link-download" download="" target="_blank" href="https://static.igem.wiki/teams/4118/wiki/website-assets/education/synthetic-biology-chapter-docx.pdf">Download synthetic biology chapter archive</a>
+          </section>
+          <section id="section7">
+            <p>Our team wrote a book about Synthetic Biology achievements so far. The first chapter is dedicated to Nobel prize winners on Synthetic Biology issues. This section provides information about scientists awarded since 1958, highlighting their research achievements. Lastly, we have included a timeline of Synthetic Biology's most significant dates and accomplishments, aiming to briefly present the history of this scientific field to the readers.</p>
+            <figure class="gallery">
+              <a href=""><img src=""></a>
+            </figure>
           </section>
         </div>
       </div>
