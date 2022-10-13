62ae74fdbd79b864ab7ca0cbbc7867092ba2af09
georgeandr <giorgosandronikidis@hotmail.com>
2022-10-14 02:47:53 +0300

    final

--- pages/parts.vue
+++ pages/parts.vue
@@ -7,6 +7,7 @@
     </div>
 
     <section class="intro">
+      <p>In this part collection which ranges from part number BBa_K4118000 to BBa_K4118008, we have successfully submitted nine basic parts to the Registry. The primary function of this collection lies with the DNA Nanostructure it entails. We have created a Nanostructure for each one of our targets (hsa_circ_0102533, hsa_circ_0005962, hsa_circ_0070354). Hence, we enlisted an H1/H2 probe couple for each target. Since all LDNs consist of the same linear scaffold, we enlisted one RCA domain, corresponding to a single repeat of the scaffold. Lastly, we enlisted the phosphorylated template and the primer from which the linear scaffold is produced through Rolling Circle Amplification. Since we aim for our technique to be implemented for detecting other circular RNAs in multiple diseases, this collection seeks to provide all the necessary info for the Nanostructure construction free-handedly.</p>
       <div class="table-responsive">
         <table class="table table-hover">
           <thead class="table-dark">
@@ -58,28 +59,28 @@
               <th scope="row">Part:BBa_K4118005</th>
               <td>hsa_circ_0102533 H2 probe</td>
               <td>DNA</td>
-              <td>Eleftherios Bochalis</td>
+              <td>Eirini Ntereki</td>
               <td><a class="link-primary" href="http://parts.igem.org/Part:BBa_K4118005">http://parts.igem.org/Part:BBa_K4118005</a></td>
             </tr>
             <tr>
               <th scope="row">Part:BBa_K4118006</th>
               <td>hsa_circ_0005962 H2 probe</td>
               <td>DNA</td>
-              <td>Eleftherios Bochalis</td>
+              <td>Eirini Ntereki</td>
               <td><a class="link-primary" href="http://parts.igem.org/Part:BBa_K4118006">http://parts.igem.org/Part:BBa_K4118006</a></td>
             </tr>
             <tr>
               <th scope="row">Part:BBa_K4118007</th>
               <td>Phosphorylated template</td>
               <td>DNA</td>
-              <td>Eleftherios Bochalis</td>
+              <td>Eirini Ntereki</td>
               <td><a class="link-primary" href="http://parts.igem.org/Part:BBa_K4118007">http://parts.igem.org/Part:BBa_K4118007</a></td>
             </tr>
             <tr>
               <th scope="row">Part:BBa_K4118008</th>
               <td>RCA primer</td>
               <td>DNA</td>
-              <td>Eleftherios Bochalis</td>
+              <td>Eirini Ntereki</td>
               <td><a class="link-primary" href="http://parts.igem.org/Part:BBa_K4118008">http://parts.igem.org/Part:BBa_K4118008</a></td>
             </tr>
           </tbody>
